69ea3669f0147f53661eca5366db7e94a381d556
timmtonga <timmtonga@gmail.com>
2015-02-24 16:16:42 +0200

    Modified priority to reflect changes in BLIS

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,8 @@ module HomeHelper
  def lab_dashboard(list)
 
   (list || []).each do |specimen|
-
+   test_priority = specimen['priority'].split(',')
+   display_priority = (((test_priority.include?'S') ? 'STAT' : ((test_priority.include?'R') ? 'RT' : 'OR'))).upcase
     @specimens << { 'priority' => 'STAT', 'test' => specimen["test_type_name"],
                     "action" => calculate_viability(specimen["time_drawn"], 10),"location"=> specimen["location"],
                     'name' => specimen['patient_name'],"accession_number" => specimen['accession_number']}
@@ -22,7 +23,7 @@ module HomeHelper
 
    life_span = specimen["lifespan"].split(',').collect{|x| x.to_i}.sort
    test_priority = specimen['priority'].split(',')
-   display_priority = (((test_priority.include?'STAT') ? 'st' : ((test_priority.include?'Routine') ? 'rt' : 'op'))).upcase
+   display_priority = (((test_priority.include?'S') ? 'STAT' : ((test_priority.include?'R') ? 'RT' : 'OR'))).upcase
     @specimens << { 'priority' => display_priority, 'orderer' => specimen['ordered_by'],
                     'status' => specimen['status'], 'department' => specimen['department'],
                     "action" => calculate_viability(specimen["time_drawn"], life_span[0]),
@@ -43,7 +44,7 @@ module HomeHelper
    act = action[test['status']]
    life_span = test["lifespan"].split(',').collect{|x| x.to_i}.sort
    test_priority = test['priority'].split(',')
-   display_priority = (((test_priority.include?'STAT') ? 'st' : ((test_priority.include?'Routine') ? 'rt' : 'op'))).upcase
+   display_priority = (((test_priority.include?'S') ? 'STAT' : ((test_priority.include?'R') ? 'RT' : 'OR'))).upcase
     @specimens << { 'priority' => display_priority,'orderer' => test['ordered_by'],
                     'status' => test['status'], 'department' => test['department'],
                     "action" => (act.is_a?(Array) ? calculate_viability(test["time_drawn"], life_span[0]) : act), 'name' => test['patient_name']}
